1ec28dcb749a774e09268246f325a529fcfd4fa0
mmhk30313 <mmhk.mmhkitsforyou@gmail.com>
2021-04-06 22:13:32 +0600

    Alhumdulillah Done

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ const DB_PASSWORD = process.env.DB_PASSWORD;
 // console.log(DB_NAME);
 const ObjectId = require('mongodb').ObjectId;
 const MongoClient = require('mongodb').MongoClient;
-const uri = `mongodb+srv://Book-Life:${DB_PASSWORD}@cluster0.zsxmj.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+// Old
+// const uri = `mongodb+srv://Book-Life:${DB_PASSWORD}@cluster0.zsxmj.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
+// Current
+const uri = `mongodb+srv://Book-Life:${DB_PASSWORD}@cluster0.vlgzx.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`;
+
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
@@ -19,10 +25,17 @@ app.get("/", (req, res)=>{
 })
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-  const bookCollection = client.db(DB_NAME).collection("all_books");
-  const userBookCollection = client.db(DB_NAME).collection("user_books");
+  // Old version -> main project
+  // const bookCollection = client.db(DB_NAME).collection("all_books");
+  // const userBookCollection = client.db(DB_NAME).collection("user_books");
+
+  // Current version -> real project
+  const bookCollection = client.db(DB_NAME).collection("book-list");
+  const userBookCollection = client.db(DB_NAME).collection("user-book-list");
+  const ordersCollection = client.db(DB_NAME).collection("order-list");
+
   // perform actions on the collection object
-  // console.log("Mongo Connected");
+  console.log("Mongo Connected");
   // Book Collections
   app.post('/addBook', (req, res) =>{
     //   console.log(req.body);
@@ -35,33 +48,34 @@ client.connect(err => {
       .catch(err=> console.log(err))
   })
   app.get('/allBooks', (req, res) =>{
+      // console.log(204)
       bookCollection.find({})
       .toArray((err, documents) => {
-        //   console.log(documents);
+          // console.log(documents);
           res.send(documents);
       })
   })
 
-  // Delete a user from MongoDB by id using (params)...
+  // Delete a book from MongoDB by id using (params)...
   app.get('/delete/:id', (deleteReq, deleteRes) =>{
     const id = deleteReq.params.id;
     bookCollection.deleteOne({_id: ObjectId(id)})
     .then(document => {
         // console.log(document.deletedCount);
-        if(document.deletedCount){
-            app.get('/allBooks', (req, res) =>{
-                bookCollection.find({})
-                .toArray((err, documents) => {
-                  //   console.log(documents);
-                    res.send(documents);
-                })
-            })
-        }
         deleteRes.send(document);
     })
   })
-
+  
   // User Collections
+  app.get('/delete/user-book/:id', (req, res) =>{
+    const id = req.params.id;
+    // console.log(id)
+    userBookCollection.deleteOne({_id: ObjectId(id)})
+    .then(document => {
+        res.send(document);
+    })
+  })
+
   app.get('/all-users-books', (req, res) =>{
     userBookCollection.find({})
     .toArray((err, documents) =>{
@@ -79,7 +93,30 @@ client.connect(err => {
     })
   })
 
-  app.post('/add-book', (req, res) =>{
+  app.get('/delete/user-books/email', (req, res) =>{
+    const userEmail = req.query.email;
+    // console.log(userEmail);
+    userBookCollection.deleteMany({userEmail})
+    .then(document => {
+      // console.log(document.deletedCount);
+      res.send(document);
+    })
+  })
+
+  app.post('/user-book/update-from-table/:id', (req, res) =>{
+    const id = req.params.id;
+    const quantity = req.body.quantity;
+    // console.log( quantity, " ", id)
+    userBookCollection.updateOne({_id: ObjectId(id)}, {
+      $set: { quantity }
+    })
+    .then(result => {
+      // console.log(result.modifiedCount);
+      res.send(result);
+    })
+  })
+
+  app.post('/user/add-book', (req, res) =>{
     const userBookData = req.body;
     // console.log(userBookData)
     userBookCollection.insertOne(userBookData)
@@ -89,7 +126,7 @@ client.connect(err => {
     .catch(err => console.log(err))
   })
 
-  app.post('/user-books/:id', (req, res)=>{
+  app.post('/user-book/update-from-book-card/:id', (req, res)=>{
     // console.log(req.params.id,"\n",req.body);
     const id = req.params.id;
     // console.log(id);
@@ -97,9 +134,9 @@ client.connect(err => {
     const userBookData = req.body;
     userBookCollection.findOne({_id: ObjectId(id), userEmail})
     .then(data => {
-      // console.log(data, " => users data");
+      // console.log("Old data => ",  data);
         data.quantity += userBookData.quantity;
-        // console.log(data);
+        // console.log("Updated data => ", data);
         userBookCollection.updateOne({_id: ObjectId(id)},{
           $set: { quantity: data.quantity }
         })
@@ -110,6 +147,48 @@ client.connect(err => {
     .catch(err => console.log(err))
   })
 
+  // Order Collection
+  app.get('/user-order',(req, res)=>{
+    const email = req.query.email;
+    ordersCollection.findOne({email})
+    .then(data => {
+      // console.log(data, " =>");
+      res.send(data);
+    })
+  })
+
+  function orderInsertion(order, email, res){
+
+    // console.log(email)
+    ordersCollection.insertOne({order, email})
+    .then(data => {
+      // console.log(data.ops[0]);
+      res.send(data.ops[0]);
+    })
+  }
+
+  app.post('/update-user/orders', (req, res) =>{
+    const order = req.body;
+    const email = req.query.email;
+    // console.log(email);
+    ordersCollection.findOne({email})
+    .then(data => {
+      // console.log(data);
+      if(!data){
+        orderInsertion(order, email, res);
+      }
+      else{
+        ordersCollection.updateOne({email},{
+          $set: {email, order}
+        })
+        .then(result => {
+          // console.log(result);
+          res.send(result);
+        })
+      }
+    })
+    
+  })
 //   client.close();
 });
 
